2da27a8be76ff323c2bd0153794d3830f960c6ad
김민성 <kmsz5813@naver.com>
2021-11-08 10:50:10 +0900

    로딩 화면 추가

---
+++ app/src/main/java/com/nepplus/librarypractice_20211105/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.nepplus.librarypractice_20211105
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setVluse()
+    }
+
+    fun setupEvents( )  {
+
+
+    }
+
+    fun setVluse( ){
+
+    }
+}
